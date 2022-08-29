f4686edb0ee823aa32d23673ca65553647c86b75
Tyler Nijmeh <tylernij@gmail.com>
2022-08-29 13:35:18 -0700

    shared_objects: Add shared string list for exercise names
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/data/local/shared_objects.dart
+++ lib/src/data/local/shared_objects.dart
@@ -1,4 +1,7 @@
 import 'package:shared_objects/shared_objects.dart';
 
 /// A container that holds objects that extend [SharedObjectBase].
-class SharedObjects {}
+class SharedObjects {
+  /// The list of saved exercise names.
+  final exercisesNames = SharedStringList('exercises_names');
+}
